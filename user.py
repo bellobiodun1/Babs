4a125481f03b7bfe16c96994f559d98755c943cc
bellobiodun1 <biodungunners@yahoo.com>
2025-04-05 13:55:49 +0100

    second update

---
+++ user.py
@@ -0,0 +1,7 @@
+def profile():
+    username= input("enter your username")
+
+    password= input("enter your user password")
+    print(f"my username is {username} and my password is {password}")
+
+profile()
